d00e973445b11394fd9a23c5757b781986a4a659
U-MCORTECH\Fintan <Fintan@M22.mcortech.local>
2017-03-09 10:43:00 +0000

    add soem edits to test.py

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
 this is a test of git
 chnaged the fil here i am addign more stuff to the file for a bufg fix
-this is more code to the file
+this is more code to the fil
+
+if(True)
+
+as =12
+
+else
+
+b=23
+
+// some comments on the code
+
